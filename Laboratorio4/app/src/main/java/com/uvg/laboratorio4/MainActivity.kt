fae5f343457cf785f73055708ffc1afc6729e58f
Poposit <che231177@uvg.edu.gt>
2024-09-23 19:17:01 -0600

    Laboratorio 4 - para trabajar el Lab5

---
+++ Laboratorio4/app/src/main/java/com/uvg/laboratorio4/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.uvg.laboratorio4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uvg.laboratorio4.ui.theme.Laboratorio4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laboratorio4Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CampusCentralScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun ScreenPreview() {
+    Laboratorio4Theme {
+        CampusCentralScreen()
+    }
+}
